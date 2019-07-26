ae6a76fbeb04366de34ae498d3fee1970c07c143
Rahul Chouhan <rahul.09chouhan1996@gmail.com>
2019-07-27 04:11:49 +0530

    Task completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GoogleChartComponent } from 'angular-google-charts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'transportsimple';
+
+  ngOnInit() {
+    this.draw();
+  }
+
+  arr = [['', 0]];
+
+  startPointName: String = "";
+  endPointName: String = "";
+
+  @ViewChild('chart')
+  chart: GoogleChartComponent;
+
+  charts: any;
+
+  public journeys = [];
+
+  public level1 = 250;
+  public level2 = 150;
+
+  public add() {
+    this.journeys.push({
+      startPoint: this.startPointName,
+      endPoint: this.endPointName
+    });
+    let x;
+    if (this.journeys.length > 0) {
+      x = `${this.startPointName.slice(0, 3).toUpperCase()}-${this.endPointName.slice(0, 3).toUpperCase()}`;
+    }
+    let y;
+    if (this.journeys.length >= 2) {
+      if (this.journeys[this.journeys.length - 1].startPoint === this.journeys[this.journeys.length - 2].startPoint &&
+        this.journeys[this.journeys.length - 1].endPoint === this.journeys[this.journeys.length - 2].endPoint) {
+        y = this.level2;
+        this.arr[this.arr.length - 1][1] = this.level2;
+      } else {
+        y = this.level1;
+      }
+    } else {
+      y = this.level1;
+    }
+    let dimensions = [x, y];
+    this.arr.push(dimensions);
+    this.draw();
+    this.startPointName = "";
+    this.endPointName = "";
+  }
+
+  public draw() {
+    this.charts = {
+      title: "Graph for journey",
+      type: 'LineChart',
+      columnNames: ['X', 'Level'],
+      data: this.arr,
+      options: {
+        hAxis: {
+          title: 'Trips'
+        },
+        vAxis: {
+          title: 'Levels',
+          direction: -1
+        }
+      },
+      height: 400,
+      width: 600
+    };
+  }
 }
